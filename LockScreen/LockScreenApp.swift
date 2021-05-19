8bb0eb9963a551c828701fd1e2e6b7f75d872056
Giorgio Giannotta <jorge_upa@msn.com>
2021-05-19 15:41:10 +0200

    Initial Commit

---
+++ LockScreen/LockScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockScreenApp.swift
+//  LockScreen
+//
+//  Created by Jorge Giannotta on 19/05/21.
+//
+
+import SwiftUI
+
+@main
+struct LockScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
